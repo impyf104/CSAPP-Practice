59e6af04287e178463dd3f972fca92ff8a30b4ce
PpPqPdPbP <impyf104@gmail.com>
2022-03-23 15:18:00 +0800

    .

--- chapter_2.cpp
+++ chapter_2.cpp
@@ -1,8 +1,54 @@
 #include<bits/stdc++.h>
 
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, int len) {
+    int i;
+    for (i = 0; i < len; i++) {
+        printf("%.2X", start[i]);
+        printf("\n");
+    }
+}
+
+void show_int(int x) {
+    show_bytes((byte_pointer) &x, sizeof(int));
+}
+
+void show_float(float x) {
+    show_bytes((byte_pointer) &x, sizeof(float));
+}
+
+void show_pointer(void *x) {
+    show_bytes((byte_pointer) &x, sizeof(void*));
+}
+
+void practice_5() {
+    int val = 0x12345678;
+    byte_pointer valp = (byte_pointer) &val;
+    show_bytes(valp, 1); /* A. */
+    show_bytes(valp, 2); /* B. */
+    show_bytes(valp, 3); /* C. */
+}
+
+void test_show_bytes(int val) {
+    int ival = val;
+    float fval = (float) ival;
+    int *pval = &ival;
+    show_int(ival);
+    printf("\n");
+    show_float(fval);
+    printf("\n");
+    show_pointer(pval);
+    printf("\n");
+}
+
+int sum(int x, int y) {
+    return x + y;    
+}
+
 int main() {
-    cout << (3.14+1e20) - 1e20 << endl;
-    cout << 3.14 + (1e20 - 1e20) << endl;
+    char *s = "12345";
+    show_bytes((byte_pointer) &s, 6);
     return 0;
 }
 
